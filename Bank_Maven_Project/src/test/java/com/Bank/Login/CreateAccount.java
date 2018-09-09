5aafa39f8420ec9ce776ae0f505be8dfa58cf16b
Administrator <Administrator@ASI-1>
2018-09-09 10:33:16 -0500

    create Account class

---
+++ Bank_Maven_Project/src/test/java/com/Bank/Login/CreateAccount.java
@@ -0,0 +1,14 @@
+package com.Bank.Login;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreateAccount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://facebook.com");
+	}
+
+}
